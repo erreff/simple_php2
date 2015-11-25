1dbce33690cb38788eb410d512176b213b853b57
unknown <EYJ673@343660ZM0037.dc.courrier.intra.laposte.fr>
2015-11-25 11:47:10 +0100

    new test batch

--- batch/testvtom.sh
+++ batch/testvtom.sh
@@ -5,6 +5,21 @@ if [ ${#} -ne 0 ];then	#{
 	CR=$1
 fi	#}
 
+WAIT_LOOP=0
+if [ ${#} -eq 2 ];then	#{
+	WAIT_LOOP=$2
+fi	#}
+
 
 echo "testvtom : exit $CR"
+
+for (( i=1; i <= $WAIT_LOOP; i++ ))
+do
+ echo "Attente totale $i secondes"
+ # Attente 1 seconde
+ sleep 1 
+done
+
+
+
 exit $CR
